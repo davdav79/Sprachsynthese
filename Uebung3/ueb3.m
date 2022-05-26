2ab369ad0c4f4039e54aec80542f8483237848a8
davdav79 <d.eichhorn1@gmx.net>
2022-05-26 15:01:54 +0200

    Aufgabe 3.3 formanten + bild

--- Uebung3/ueb3.m
+++ Uebung3/ueb3.m
@@ -3,9 +3,9 @@ home    % verschiebt inhalt im command window, alternatic clc(löschen und nicht
 close all   % schließt alle grafikfenster
 
 % Einlesen der wave-files
-filename1 = "david/Satz_David.wav"
-filename2 = "marc/Satz_Marc.wav"
-filename3 = "stefan/Satz_Stefan.wav"
+filename1 = "david/Satz_David.wav";
+filename2 = "marc/Satz_Marc.wav";
+filename3 = "stefan/Satz_Stefan.wav";
 [sk1,Fs] = audioread(filename1);
 [sk2,Fs] = audioread(filename2);
 [sk3,Fs] = audioread(filename3);
@@ -23,7 +23,25 @@ prob3 = hist3/sum(hist3)*100;
 % Plot
 plot(x1,prob1,x2,prob2,x3,prob3)
 
+
 legend({'Satz David','Satz Marc','Satz Stefan'},'Location','northeast')
 xlim([-1 1])
 xlabel("Amplituden-Wertebereich")
 ylabel("Häufigkeit in %")
+
+% Plot David Formanten
+figure()
+TDavid = readtable("david/formant.csv","NumHeaderLines", 1)
+TMarc = readtable("marc/formant.csv","NumHeaderLines", 1)
+TStefan = readtable("stefan/formant.csv","NumHeaderLines", 1)
+
+plot(TDavid.Var3,TDavid.Var2,'.',TMarc.Var3,TMarc.Var2, '.',TStefan.Var3,TStefan.Var2, '.')
+title("Formanten Tabelle David, Marc, Steffan")
+set(gca,'XAxisLocation','top','YAxisLocation','right','ydir','reverse', 'xdir', 'reverse');
+xlabel("F2")
+ylabel("F1")
+legend("David","Marc","Stefan")
+text(TDavid.Var3-20,TDavid.Var2-10,TDavid.Var1)
+text(TMarc.Var3-20,TMarc.Var2-10,TMarc.Var1)
+text(TStefan.Var3-20,TStefan.Var2-10,TStefan.Var1)
+
